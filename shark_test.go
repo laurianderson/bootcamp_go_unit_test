3b213a95cceea03024858f0819a5b360a07e4ec3
laurianderson <laura.anderson@mercadolibre.com>
2023-05-08 13:08:47 -0300

    feat: unit test: TestSharkCannotHuntBecauseIsTired

--- shark_test.go
+++ shark_test.go
@@ -1,13 +1,35 @@
 package hunt
 
 import (
+	"fmt"
 	"testing"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSharkHuntsSuccessfully(t *testing.T) {
 }
 
 func TestSharkCannotHuntBecauseIsTired(t *testing.T) {
+	// arrange
+	shark := Shark{
+		hungry: false,
+		tired: true,
+		speed: 20,
+	}
+
+	prey := Prey{
+		name: "fish",
+		speed: 10,
+	}
+
+	response := fmt.Errorf("cannot hunt, i am really tired")
+
+	// act
+	err := shark.Hunt(&prey)
+
+	// assert
+	assert.Equal(t, response.Error(), err.Error())
+
 }
 
 func TestSharkCannotHuntBecaisIsNotHungry(t *testing.T) {
